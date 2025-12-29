47b3261d51d0c16efcf8daf5a9b302d2620decb4
DVSS SUSHWANTH VARMA CH <saisushwanth17@gmail.com>
2025-12-29 12:44:34 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,21 @@
+public class Operations
+{
+public static void main(String[] args)
+{
+int Num1=55, Num2=56, Num3=62,Num4=6;
+boolean Num5=true;
+int Num;
+//ARITHMETIC OPERATOR
+Num=(Num1+Num2)-Num3/2;
+System.out.println(Num);
+//UNARY OPERATOR
+System.out.println(Num1++);
+//RELATIONAL OPERATOR
+System.out.println(Num3>=Num1+Num2);
+System.out.println(Num2==Num1);
+//LOGICAL OPERATOR
+System.out.println((Num2<Num3)&&(Num1>Num3));
+System.out.println(!Num5);
+}
+}
+
