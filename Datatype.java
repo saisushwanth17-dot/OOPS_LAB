6907e789ee0de8c0d2cb3670089d48bafa0b8c9d
DVSS SUSHWANTH VARMA CH <saisushwanth17@gmail.com>
2025-12-29 12:12:31 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,31 @@
+public class Datatype
+{
+public static void main(String[] args)
+{
+boolean isJavaFun = true;
+System.out.println("Boolean (isJavaFun): " + isJavaFun);
+
+byte aByte = 100;
+System.out.println("Byte (aByte): " + aByte);
+
+short aShort = 5000;
+System.out.println("Short (aShort): " + aShort);
+
+int anInt = 100000;
+System.out.println("Int (anInt): " + anInt);
+
+long aLong = 15000000000L;
+System.out.println("Long (aLong): " + aLong);
+
+float aFloat = 23.45f;
+System.out.println("Float (aFloat): " + aFloat);
+
+double aDouble = 123.456789;
+System.out.println("Double (aDouble): " + aDouble);
+
+char aChar = 'A';
+System.out.println("Char (aChar): " + aChar);
+
+}
+   
+}
