a105211fd00be55ebd20a843f4e143b38f076383
DVSS SUSHWANTH VARMA CH <saisushwanth17@gmail.com>
2026-03-23 12:29:59 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
-public class Main{
-public static void main(String[] args){
-System.out.print("HI");
+class Animal {
+    public void sound() {
+        System.out.println("Animal makes a sound");
+    }
 }
+
+class Dog extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        Animal a3 = new Animal();
+
+        a1.sound();
+        a2.sound();
+        a3.sound();
+    }
 }
